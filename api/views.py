7d7ed40c9307853140fb035ae11995d60c54112c
Linh Pham <thuytpham9@gmail.com>
2020-01-21 22:07:25 -0800

    Created rate_product method in views.py

--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import Product, Rating
 from .serializer import ProductSerializer, RatingSerializer
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def rate_product(self, request):
+        response = {'message': 'its ok'}
+        return Response(response, status=status.HTTP_200_OK)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
