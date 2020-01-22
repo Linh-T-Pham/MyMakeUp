c14e6e9ef29cf41522bd4825a6e6297eaa26100d
Linh Pham <thuytpham9@gmail.com>
2020-01-21 22:18:58 -0800

    Updated views.py

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Product, Rating
 from .serializer import ProductSerializer, RatingSerializer
 
@@ -8,9 +9,20 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    def rate_product(self, request):
-        response = {'message': 'its ok'}
-        return Response(response, status=status.HTTP_200_OK)
+    # what kind of method
+    @action(detail=True, methods=['POST'])
+    def rate_product(self, request, pk=None):
+        if 'stars' in request.data:
+
+            product = Product.objects(id=pk)
+            print('movie title', movie.title)
+
+
+            response = {'message': 'awesome'}
+            return Response(response, status=status.HTTP_200_OK)
+        else:
+            response = {'message': 'Please provide stars'}
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
