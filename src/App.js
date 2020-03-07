9bd28e7385317a22df7906fde3971cd19c45b9a8
linh <11181988@laptop-t32km911.plsinfo.org>
2020-03-06 16:37:51 -0800

    Fetching data from API

--- src/App.js
+++ src/App.js
@@ -7,10 +7,18 @@ class App extends Component {
   products = ['Eyebrow', 'Foundation', 'Eyeliner', 'Lip Gross'];
 
   componentDidMount(){
-    //fetch data 
+    //fetch data
+    fetch('http://127.0.0.1:8000/api/products/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 2a795c60cee426a6483657e2857d3bb08c8c4fd6'
+      }
+    }).then( resp => console.log(resp))
+    .catch( error => console.log(error))
   }
   render(){
     return (
+
       <div className="App">
           <h1>MyMakeUP</h1>
           <ProductList products={this.products}/>
--- src/components/product-list.js
+++ src/components/product-list.js
@@ -1,10 +1,11 @@
 import React from 'react';
+
 function ProductList(props){
     
     return (
         <React.Fragment>
         { props.products.map( product => {
-            return <h3>{product}</h3>
+            return <h3 key={product}>{product}</h3>
         })}
         </React.Fragment>
     )
